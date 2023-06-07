8675064921dc864d841cac5acdd5917730d47358
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2023-06-07 17:32:51 -0600

    knn_initial

--- assets/modules/PE10/src/knn_wrapper.py
+++ assets/modules/PE10/src/knn_wrapper.py
@@ -1,3 +1,13 @@
+""" knn_wrapper.py
+
+_summary_
+
+_extended_summary_
+
+Returns:
+    _type_: _description_
+"""
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import datasets
 import pandas as pd
@@ -5,7 +15,27 @@ import csv
 
 
 class knnWrapperClass:
+    """
+    Summary:
+    This class provides methods for training a KNN classifier, making predictions, and importing data files for training and testing.
+
+    _extended_summary_
+    """
+
     def __init__(self):
+        """
+        Initialize the KNNWrapperClass.
+
+        Summary:
+            This method initializes the KNNWrapperClass object by setting initial values for the KNN classifier and data variables.
+
+        Extended Summary:
+            - knn: An instance of the KNeighborsClassifier class from scikit-learn.
+            - df_train: Pandas DataFrame for training data.
+            - df_train_target: Pandas DataFrame for training targets.
+            - df_test: Pandas DataFrame for test data.
+            - df_test_target: Pandas DataFrame for test targets.
+        """
         knn = None
         df_train = None
         df_train_target = None
@@ -13,23 +43,60 @@ class knnWrapperClass:
         df_test_target = None
 
     def train(self, neighbors):
-        """...write comment about this method...
+        """
+        Train the KNN classifier.
+
+        Summary:
+            This method trains the KNN classifier with the specified number of neighbors.
+
+        Extended Summary:
+            Args:
+                - neighbors (int): The number of neighbors to consider for classification.
         """
         # TODO: function logic
 
+
     def predict(self):
-        """...write comment about this method...
+        """
+        Make predictions using the trained KNN classifier.
+
+        Summary:
+            This method makes predictions using the trained KNN classifier on the test data.
+
+        Extended Summary:
+            Returns:
+                - type: Description of the return value.
         """
         # TODO: function logic
 
         return 0
 
-    def import_train_file(self, trainDataFile, trainTargetFile):
-        """...write comment about this method...
+
+    def import_train_file(self, train_data, train_tgt):
+        """
+        Import training data and target files.
+
+        Summary:
+            This method imports the training data and target files from the specified paths.
+
+        Extended Summary:
+            Args:
+                - trainDataFile (str): Path to the training data file.
+                - trainTargetFile (str): Path to the training target file.
         """
         # TODO: function logic
 
+
     def import_test_file(self, testDataFile, testTargetFile):
-        """...write comment about this method...
+        """
+        Import test data and target files.
+
+        Summary:
+            This method imports the test data and target files from the specified paths.
+
+        Extended Summary:
+            Args:
+                - testDataFile (str): Path to the test data file.
+                - testTargetFile (str): Path to the test target file.
         """
         # TODO: function logic
--- assets/modules/PE10/src/knn_wrapper_main.py
+++ assets/modules/PE10/src/knn_wrapper_main.py
@@ -1,22 +1,30 @@
 """knn_wrapper_main.py
 
 This assignment is to learn about k-nearest neighbors.
-Given two data sets, complete the functions in the separate "knnWrapperClass" class within knn_wrapper.py,
-and edit the parameter on the "train" method call from the main method to achieve 100% accuracy.
-The "knnWrapperClass" object is to implement a k-nearest classifier wrapper and classify test data.
-Note that you are allowed to use the KNeighborsClassifier library from sklearn, and you need to achieve a 100% classification rate on test data.
-In short paragraph, provide which 'neighbor' parameter works best for each dataset and describe tips on choosing the 'neighbor' value.
 
--Files are in pair of "data" and "target," where their lows are synced, and each data low is labeled with classes at the target file.
-  Train files: iris_data.csv, iris_target.csv, wine_data.csv, wine_target.csv
-  Test files: iris_test_data.csv, iris_test_target.csv, wine_test_data.csv, wine_test_target.csv
+Given two data sets, complete the functions in the separate "knnWrapperClass"
+class within knn_wrapper.py, and edit the parameter on the "train" method call
+from the main method to achieve 100% accuracy. The "knnWrapperClass" object is
+to implement a k-nearest classifier wrapper and classify test data. Note that
+you are allowed to use the KNeighborsClassifier library from sklearn, and you
+need to achieve a 100% classification rate on test data. In a short paragraph,
+provide which 'neighbor' parameter works best for each dataset and describe
+tips on choosing the 'neighbor' value.
 
--Result should achieve five 0s, five 1s, five 2s:
+Files are in pair of "data" and "target," where their lows are synced, and each
+data low is labeled with classes at the target file.
+  Train files:
+    iris_data.csv, iris_target.csv, wine_data.csv, wine_target.csv
+  Test files:
+    iris_test_data.csv, iris_test_target.csv,
+    wine_test_data.csv, wine_test_target.csv
+
+Result should achieve five 0s, five 1s, five 2s:
   [0 0 0 0 0 1 1 1 1 1 2 2 2 2 2]
   [0 0 0 0 0 1 1 1 1 1 2 2 2 2 2]
-
 
 """
+
 from knn_wrapper import knnWrapperClass
 
 
